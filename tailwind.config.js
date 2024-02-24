73116ecf8b602bb6c0fb7d5e2784d47febec6362
João Diego Florencio <jdflorencio@gmail.com>
2024-02-24 11:29:55 -0300

    first

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Defina as cores do modo escuro aqui
+        darkBackground: '#1a202c',
+        darkText: '#ffffff',
+        darkAccent: '#4fd1c5',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['dark'],
+      textColor: ['dark'],
+      borderColor: ['dark'],
+    },
+  },
+  plugins: [],
+}
+
