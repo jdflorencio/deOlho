51ad055a532c506cdeed2b30cb0f7cf7be348083
João Diego Florencio <jdflorencio@gmail.com>
2024-02-25 00:45:24 -0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { HomeComponent } from './resources/pages/home/home.component';
 import { SobreComponent } from './resources/pages/sobre/sobre.component';
 import { MenuModule } from './resources/components/menu/menu.module';
-
-//import { fron } from '@fortawesome/fontawesome-free';
-
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, SobreComponent],
   imports: [
-    BrowserModule, 
-    AppRoutingModule, 
+    BrowserModule,
+    AppRoutingModule,
     MenuModule,
-    FontAwesomeModule
-    
-    //FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
